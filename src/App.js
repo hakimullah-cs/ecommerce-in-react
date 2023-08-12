ec379877625e1a9ae099dc7c47e3ea1e120fe394
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2023-08-12 22:32:25 +0500

    Setup the project and install tailwindcss

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+
+const Layout=()=>{
+  return (
+    <div className="app overflow-x-hidden">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Layout/>,
+    children:[
+      {
+        path:"/",
+        element:'home'
+      },
+      
+      
+     
+    ]
+  },
+ 
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
